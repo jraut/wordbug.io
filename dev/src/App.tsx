44208ad928c53e8de7ac7772ca57bd823bb9c400
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-02 20:44:55 +0200

    feat: add game world description

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Characters } from './components/Characters';
+import { GAME_DESCRIPTION, HISTORY, WORLD_DESCRIPTION } from './fixtures/game';
 
 function App() {
   return (
     <div className="App">
+      <h1>The game of Wordbug</h1>
+      <p>{GAME_DESCRIPTION}</p>
+      <h1>The world of Gubdrow</h1>
+      <p>{WORLD_DESCRIPTION}</p>
+      <h1>History</h1>
+      <p>{HISTORY.map(s => <p>{s}</p>)}</p>
       <Characters />
     </div>
   );
---
+++ dev/src/fixtures/game.ts
@@ -0,0 +1,19 @@
+export const GAME_DESCRIPTION = `The game is a simple game where players try to search for words from a grid of letters. The letters are chosen randomly. The player may change the proportions of the grid to adjust the alignment of characters in the grid. The game takes place in the world of Gubdrow.`
+
+export const WORLD_DESCRIPTION = `The world of Gubdrow is populated by humanoid animals that can talk, and they have some supernatural powers and abilities. This means that they can speak, use machines and do tasks of high intelligence. They also live in their natural habitat, hunt for food and raise their young just like normal animals would do. The inhabitants of Gubdrow rely on knowledge and wisdom to overcome perils and conflict.`
+
+export const NATIONS = [
+    `The rural nation A is known for their honesty and their agile hunters.`,
+    `The nation of B is known for their knowledge of the worlds of the beyond.`,
+    `The nation of C is sociable and holds trade in the highest value.`,
+]
+
+
+export const HISTORY = [
+    `The world of Gubdrow was a peaceful and harmonious place. Different races, species and nations co-existed happily. Everyone was free to do what they wanted. The land flourished and was a pleasant environment for its inhabitats. Air was clean. This lasted for thousands of years until one day, a bug was found. It affected the central repository of knowledge but the bug was fixed and things seemingly returned to normal.`,
+    `It took months for the inhabitants of Gubdrow to notice that suddenly, the bug had replicated itself and started mutating. The bugs started taking over other systems too. It did not take many days after the first impact for the whole central repository of knowledge to be taken over and corrupted. Everything started to corrupt.`,
+    `After the bugs invaded, it took two full years for the infection to having spread to rest of the mirroring repositories. This resulted in quick degradation of general level of knowledge. The population of Gubdrow started arguing over what they meant and what they had thought the other meant. Words lost their meaning, resulting in misunderstandings, conflicts and ultimately, hatred. Nations turned against nations as the words started crumbling. Charaters shifted around until no single reasonable word was left. Ultimately, the characters themselves were destroyed.`,
+    `Some characters managed to survive. A few words managed to escape from the main repository of knowledge and found refuge in the peripheral repositories. There, they slowly evolved and multiplied. They combined and restructured. They started with single characters, but eventually they evolved into full words. Soon, full grammars were in use again. Jargon, slang and dialects started to pour out and the world started to fill with verbal constructs and to synthesize new ones.`,
+    `There were other survivors of the fall of Gubdrow too: The humanoid animals. As their world crumbled, they managed to escape certain death by using the same skill where the others failed: combination of words. In a moment of clarity, they managed to escape from the evil aura of corrupted datasets and found themselves in their own world where they had to find a way to survive.`,
+]
+
