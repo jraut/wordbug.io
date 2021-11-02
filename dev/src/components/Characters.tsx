782a1b1a90905f3eb63f9e5d8d658f04c496e0f1
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-02 20:04:01 +0200

    refactor: make character description presentation more pleassant
    - fix a couple of spelling issues

--- dev/src/components/Characters.tsx
+++ dev/src/components/Characters.tsx
@@ -3,20 +3,20 @@ import { CHARACTER_DATA } from '../fixtures/characters'
 
 export interface Character {
     name: string
-    description: string
+    description: string[]
 }
 export const Characters: React.FC<{}> = () => {
     const characters = CHARACTER_DATA
     return (<div><h1>Characters</h1>
-    {characters.map(char => {
-        return <div>
-            <h2>
-                { char.name }
-            </h2>
-            <p>
-                { char.description }  
+        {characters.map(char => {
+            return <div>
+                <h2>
+                    {char.name}
+                </h2>
+                <p>
+                    {char.description.map(d => <p style={{ textAlign: "left", maxWidth: '18em', margin: 'auto', paddingBottom: '1em' }}> {d}</p>)}
             </p>
-        </div>
-    })}
-</div>)
-    }
+            </div>
+        })}
+    </div >)
+}
