42c9e2cf9e3ad78365ae4a756f8c9fe1a4995290
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-03 22:14:53 +0200

    chore: add tooling

---
+++ dev/.prettierrc.js
@@ -0,0 +1,18 @@
+// Prettier code formatting config. See https://prettier.io/docs/en/options.html for details
+
+module.exports = {
+  // Specify the line length that the printer will wrap on.
+  printWidth: 80,
+  // Specify the number of spaces per indentation-level.
+  tabWidth: 2,
+  // Indent lines with tabs instead of spaces
+  useTabs: false,
+  // Print semicolons at the ends of statements.
+  semi: false,
+  // Use single quotes instead of double quotes
+  singleQuote: true,
+  // Print trailing commas wherever possible when multi-line. (A single-line array, for example, never gets trailing commas.)
+  trailingComma: 'all',
+  // Include parentheses around a sole arrow function parameter
+  arrowParens: 'always',
+}
