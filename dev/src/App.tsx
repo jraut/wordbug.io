742e351cb6d97dd72c7584140947f0b081856068
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-03 15:33:05 +0200

    chore: remove unused code

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Characters } from './components/Characters';
 import { GAME_DESCRIPTION, HISTORY, WORLD_DESCRIPTION } from './fixtures/game';
