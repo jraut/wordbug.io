1dca28ff8b8562c4eac9b75b6993db469298c2b5
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-03 23:23:40 +0200

    chore: formatting

--- dev/src/components/Characters.tsx
+++ dev/src/components/Characters.tsx
@@ -2,21 +2,36 @@ import React from 'react'
 import { CHARACTER_DATA } from '../fixtures/characters'
 
 export interface Character {
-    name: string
-    description: string[]
+  name: string
+  description: string[]
 }
-export const Characters: React.FC<Character> = () => {
-    const characters = CHARACTER_DATA
-    return (<div><h1>Characters</h1>
-        {characters.map(char => {
-            return <div>
-                <h2>
-                    {char.name}
-                </h2>
-                <p>
-                    {char.description.map(d => <p style={{ textAlign: "left", maxWidth: '18em', margin: 'auto', paddingBottom: '1em' }}> {d}</p>)}
+export const Characters: React.FC = () => {
+  const characters = CHARACTER_DATA
+  return (
+    <div>
+      <h1>Characters</h1>
+      {characters.map((char) => {
+        return (
+          <div>
+            <h2>{char.name}</h2>
+            <p>
+              {char.description.map((d) => (
+                <p
+                  style={{
+                    textAlign: 'left',
+                    maxWidth: '18em',
+                    margin: 'auto',
+                    paddingBottom: '1em',
+                  }}
+                >
+                  {' '}
+                  {d}
+                </p>
+              ))}
             </p>
-            </div>
-        })}
-    </div >)
+          </div>
+        )
+      })}
+    </div>
+  )
 }
