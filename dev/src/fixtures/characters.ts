4b8884e8b14a735776997c72b1f3aee84353d94e
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-05 12:42:53 +0200

    feat: add character colors to characters
    - tweak the colors

--- dev/src/fixtures/characters.ts
+++ dev/src/fixtures/characters.ts
@@ -8,6 +8,14 @@ export const CHARACTER_DATA: DraftCharacter[] = [
       `Rugo was taught to hunt by his father. As he was growing up, he realized that something was wrong with the world. The flowers began to wither, the bugs began to multiply and the balance of nature was disrupted. Rugo decided to find out what was happening. He went to explore the world.`,
       `The inhabitants of Gubdrow were losing their natural power and ability to feel joy. They were becoming cynical and full of hatred. Rugo didn't know what to do. Then he came to the conclusion that he should do something to save the world. He took his weapons and started to hunt down the bugs. One by one, he eliminated all the threats. His travels took him through the whole continent. He wanted to be a legend for his people. Rugo wanted to bring back the balance of nature.`,
     ],
+    colorsDark: {
+      main: '#be3819',
+      secondaries: ['#142f0f', '#2e4a5b', '#94679C'],
+    },
+    colorsLight: {
+      main: '#d2a785',
+      secondaries: ['#87b251', '#a1d5cf', '#c476aa'],
+    },
   },
   {
     name: 'Celso',
@@ -15,6 +23,14 @@ export const CHARACTER_DATA: DraftCharacter[] = [
       `Celso is a squirrel. He comes from the Mystic Nation which is known for their knowledge of the worlds of beyond. He lost his parents when he was young. His brother was responsible for his upbringing and he brought him to the nation of his ancestors. They had to part ways when his brother was needed to defend the nation. He always felt guilty for not being able to help his brother in time, but he knew he was doing what was best for him. Celso was lucky that there were many wise scholars there who took him in.`,
       `After having mastered his studies, Celso decided to bring peace to his nation. He used his psychic powers to help all the families who were suffering from the loss of proper words for their loved ones. He started using the forgotten old knowledge to reduce the confusion. The nation was shocked at first, but once they found out that they could use this new method to bring back understanding between each other. This new method worked and restored happiness to all of them. `,
     ],
+    colorsDark: {
+      main: '#4a7518',
+      secondaries: ['#2d6a75', '#8270a3', '#d52f25'],
+    },
+    colorsLight: {
+      main: '#d4e19e',
+      secondaries: ['#a1d5cf', '#44b3df', '#5B4998'],
+    },
   },
   {
     name: 'Mira',
@@ -23,7 +39,16 @@ export const CHARACTER_DATA: DraftCharacter[] = [
       `One day when Mira was a child, he found a nice place to make a camp. He was looking for firewood to start a fire. Suddenly, a very strong wind came, and he lost his balance. He fell down the cliff. After the fall, he fainted. A couple of days later, when he awoke, he noticed that he has been hurt badly. He had many broken bones. His right hand was completely crushed. Mira, however, had a very strong will. He took his crushed hand and got it replaced by a mechanical device he had been building. Initially recovery was slow and rebuilding the surroundings cumbersome but slowly Mira got back on his feet.`,
       `Mira had to learn to cope with his physical conditions. He went through many hardships but overcame them with technical solutions. The people in his home town were not accepting him at first, since he was not able to do networking anymore, having lost his mobility and charisma. As his physical abilities were damaged, he developed his brain instead. He learnt fast and soon became smarter than his peers. His perseverance and technical brilliance earned him the respect of his fellow citizens.`,
     ],
+    colorsDark: {
+      main: '#007788',
+      secondaries: ['#48484b', '#c8393b', '#50590d'],
+    },
+    colorsLight: {
+      main: '#7bc6c0',
+      secondaries: ['#5b5b5e', '#ea949d', '#f2edbe'],
+    },
   },
+  
 ]
 
 export const CHARACTER_COLORS = [
