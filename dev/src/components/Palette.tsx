fd0aec3567d62f1c0b2d26be8bbee40c5dc25627
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-10 01:44:28 +0200

    refactor: remove inline styles

--- dev/src/components/Palette.tsx
+++ dev/src/components/Palette.tsx
@@ -7,8 +7,8 @@ interface Palette {
 
 export const Palette: React.FC<Palette> = ({ main, secondaries }) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <div style={{ background: main, padding: '1em', marginBottom: '0.25em' }}>
+    <div className="flex-col">
+      <div style={{ background: main }} className="p-1 mb-1">
         {main}&nbsp;
       </div>
       {secondaries.map((secColorHex) => (
