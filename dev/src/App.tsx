b31b3f816ca0bfe6191a340e9f2198542c1376e3
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-10 00:18:58 +0200

    feat: add styling for layout

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -7,15 +7,13 @@ import { Description } from './components/Description';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Description header="The game of Wordbug" content={
-        GAME_DESCRIPTION} />
-      <Description header="The world of Gubdrow" content={
-        WORLD_DESCRIPTION} />
-      <Description header="History" content={
-        HISTORY} />
+      <h1 className="text-xl p-10">Wordbug</h1>
+      <Description header="The game of Wordbug" content={GAME_DESCRIPTION} />
+      <Description header="The world of Gubdrow" content={WORLD_DESCRIPTION} />
+      <Description header="History" content={HISTORY} />
       <Characters />
     </div>
-  );
+  )
 }
 
 export default App;
--- dev/src/components/Characters.tsx
+++ dev/src/components/Characters.tsx
@@ -11,53 +11,77 @@ export interface Character {
   family: Family
 }
 
-const CharactersColorDisplay: React.FC<{ characters: Character[] }> = ({ characters }) => {
+const CharactersColorDisplay: React.FC<{ characters: Character[] }> = ({
+  characters,
+}) => {
   const swatches: ReactElement[] = []
-  { characters.forEach(
-    ({ name, colors }) => {
-      swatches.push(<div style={{ background: colors.main }}>{name}:{' '}{colors.main}</div>)
-    }
-    , []) }
-  return (<div>
-  <h2>Main colors of characters</h2>
-  {swatches}
-
-    </div>)
+  {
+    characters.forEach(({ name, colors }) => {
+      swatches.push(
+        <div style={{ background: colors.main }}>
+          {name}: {colors.main}
+        </div>,
+      )
+    }, [])
+  }
+  return (
+    <div>
+      <h2>Main colors of characters</h2>
+      {swatches}
+    </div>
+  )
 }
 
 export const Characters: React.FC = () => {
   const characters = CHARACTER_DATA
   return (
     <div>
-      <h1>Characters</h1>
-      {characters.map((char) => {
-        return (
-          <div key={char.name}>
-            <h2>{char.name}</h2>
-            <p>Alignment: {char.alignment}</p>
-            <p>Family: {char.family}</p>
-            <h3>Colours</h3>
-            <Palette {...char.colors} />
-            <h3>History</h3>
-            <div> 
-              {char.description.map((d, i) => (
-                <p
-                  key={i}
-                  style={{
-                    textAlign: 'left',
-                    maxWidth: '18em',
-                    margin: 'auto',
-                    paddingBottom: '1em',
-                  }}
-                >
-                  {' '}
-                  {d}
-                </p>
-              ))}
+      <h1 className="text-xl font-large text-black">Characters</h1>
+      <div className="grid sm:grid-cols-2 xl:grid-cols-4">
+        {characters.map((char) => {
+          return (
+            <div key={char.name} className="p-4">
+              <div className="mx-auto flex flex-col space-y-4 p-2 rounded-s shadow-xl  bg-gray-300">
+                <div className="p-2 rounded-s shadow-sm items-center">
+                  <h2 className="text-xl font-large font-serif shadow-sm bg-gray-200">
+                    {char.name}
+                  </h2>
+                  <table className="text-left mx-auto pd-2">
+                    <tr>
+                      <td>Alignment: </td>
+                      <td>{char.alignment}</td>
+                    </tr>
+                    <tr>
+                      <td>Family: </td>
+                      <td>{char.family}</td>
+                    </tr>
+                  </table>
+                </div>
+
+                <div className="p-2 rounded-s shadow-sm items-center">
+                  <Palette {...char.colors} />
+                </div>
+                <div className="p-2 rounded-s shadow-sm items-center">
+                  <h3>History</h3>
+                  <div>
+                    {/* style={{ display: 'flex' }}> */}
+                    {char.description.map((d, i) => (
+                      <p
+                        key={i}
+                        className="max-w-sm mx-auto p-2 font-serif text-left"
+                      >
+                        {' '}
+                        {d}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
+
       <CharactersColorDisplay characters={characters} />
     </div>
   )
