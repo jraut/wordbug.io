30187579bbe3634916765d15f9c7bd8cc29f6c9d
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-10 02:13:18 +0200

    refactor: move app content to separate view

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import './App.css'
-import { Characters } from './components/Characters'
-import { GAME_DESCRIPTION, HISTORY, WORLD_DESCRIPTION } from './fixtures/game'
-import { Description } from './components/Description'
+import { FullLoreView } from './components/FullLoreView'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1 className="text-xl p-10">Wordbug</h1>
-      <Description header="The game of Wordbug" content={GAME_DESCRIPTION} />
-      <Description header="The world of Gubdrow" content={WORLD_DESCRIPTION} />
-      <Description header="History" content={HISTORY} />
-      <Characters />
+      <FullLoreView />
     </div>
   )
 }
