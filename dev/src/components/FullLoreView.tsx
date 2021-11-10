ca5e200981b513e96d41695da579c847f384b237
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-10 02:13:18 +0200

    refactor: move app content to separate view

---
+++ dev/src/components/FullLoreView.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { GAME_DESCRIPTION, HISTORY, WORLD_DESCRIPTION } from '../fixtures/game'
+import { Characters } from './Characters'
+import { Description } from './Description'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface FullLoreView {}
+export const FullLoreView: React.FC<FullLoreView> = () => {
+  return (
+    <div>
+      <h1 className="text-xl p-10">Wordbug</h1>
+      <Description header="The game of Wordbug" content={GAME_DESCRIPTION} />
+      <Description header="The world of Gubdrow" content={WORLD_DESCRIPTION} />
+      <Description header="History" content={HISTORY} />
+      <Characters />
+    </div>
+  )
+}
