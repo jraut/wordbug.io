4bd467500bb1267119be44bf8250a4cefb6afcf9
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-12 00:39:04 +0200

    feat: add poc resize handler

--- dev/src/components/GameArea.tsx
+++ dev/src/components/GameArea.tsx
@@ -1,17 +1,76 @@
-import { FC } from 'react'
+import React, { FC, MutableRefObject, useRef, useState } from 'react'
 import { Grid } from './Grid'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Draggable {
+  parentRef: MutableRefObject<HTMLDivElement | null>
+  onDrag: (t: Coordinates) => void
+  initialPosition: number //
+}
+
+export interface Coordinates {
+  x: number
+  y: number
+}
+export const Draggable: FC<Draggable> = ({
+  parentRef,
+  onDrag,
+  initialPosition,
+}) => {
+  // const [dragStart, setDragStart] = useState<Coordinates | undefined>()
+  const [position, setPosition] = useState<number>(initialPosition)
+
+  const handleDrag = (e: React.DragEvent<HTMLDivElement>): void => {
+    const { buttons, clientX, clientY } = e
+    if (buttons === 0 && clientX !== 0 && clientY !== 0) {
+      const { offsetTop: y, offsetLeft: x } = e.currentTarget
+      console.log(e)
+      console.log({ position, x })
+      const newPosition = clientX - (parentRef?.current?.offsetLeft ?? 0)
+      setPosition(newPosition)
+      onDrag({
+        x: newPosition,
+        y,
+      })
+      console.log(parentRef.current?.offsetLeft)
+    }
+  }
+
+  return (
+    <div
+      draggable
+      className="absolute"
+      style={{ left: `${position}px`, top: '-1em' }}
+      onDrag={handleDrag}
+    >
+      dragme
+    </div>
+  )
+}
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface GameArea {}
 
 export const GameArea: FC<GameArea> = () => {
+  const [width, setWidth] = useState(400)
+  const containerRef = useRef<HTMLDivElement>(null)
+  const resizeWidth = ({ x, y: _y }: Coordinates): void => {
+    setWidth(x)
+  }
   return (
-    <div>
-      <Grid
-        characters={Array.from(Array(600)).map((_, i) => String(i % 10))}
-        width={600}
-        height={2000}
-      />
+    <div className="flex">
+      <div ref={containerRef} className="relative mx-auto">
+        <Draggable
+          parentRef={containerRef}
+          onDrag={resizeWidth}
+          initialPosition={width}
+        />
+        <Grid
+          characters={Array.from(Array(200)).map((_, i) => String(i % 10))}
+          width={width}
+          height={800}
+        />
+      </div>
     </div>
   )
 }
--- dev/src/components/Grid.tsx
+++ dev/src/components/Grid.tsx
@@ -6,7 +6,8 @@ export interface Grid {
   width: number
   height: number
 }
-
+const easings = ['ease-linear', 'ease-in', 'ease-out', 'ease-in-out']
+const nEasings = easings.length
 export const Grid: FC<Grid> = ({ characters, width, height }) => {
   const nx = Math.ceil(Math.sqrt((characters.length * width) / height))
   let ny = Math.ceil(Math.sqrt((characters.length * height) / width))
@@ -19,24 +20,27 @@ export const Grid: FC<Grid> = ({ characters, width, height }) => {
       className="flex flex-wrap relative inset-o mx-auto"
       style={{ width: `${width}px`, height: `${height}px` }}
     >
-      {characters.map((char, i) => (
-        <div
-          key={i}
-          className="flex p-1 absolute"
-          style={{
-            width: `${blockDimension}px`,
-            height: `${blockDimension}px`,
-            top:
-              blockDimension *
-              Math.floor(i / Math.ceil(characters.length / ny)),
-            left: blockDimension * (i % nx),
-          }}
-        >
-          <div className="flex m-auto w-full h-full bg-gray-300">
-            <span className="m-auto">{char}</span>
+      {characters.map((char, i) => {
+        const easing = easings[i % nEasings]
+        return (
+          <div
+            key={i}
+            className={`flex p-1 absolute transition-all duration-700 ${easing}`}
+            style={{
+              width: `${blockDimension}px`,
+              height: `${blockDimension}px`,
+              top:
+                blockDimension *
+                Math.floor(i / Math.ceil(characters.length / ny)),
+              left: blockDimension * (i % nx),
+            }}
+          >
+            <div className="flex m-auto w-full h-full bg-gray-300">
+              <span className="m-auto">{char}</span>
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   )
 }
