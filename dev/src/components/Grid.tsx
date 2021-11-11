e94143aeb229a70d85b65d2ef6da415da87f7cd4
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-11 02:51:12 +0200

    feat: add grid component and game area for testing it

---
+++ dev/src/components/Grid.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Grid {
+  characters: string[]
+  width: number
+  height: number
+}
+
+export const Grid: FC<Grid> = ({ characters, width, height }) => {
+  const nx = Math.ceil(Math.sqrt((characters.length * width) / height))
+  let ny = Math.ceil(Math.sqrt((characters.length * height) / width))
+  if (nx * (ny - 1) > characters.length) {
+    ny = ny - 1
+  }
+  const blockDimension = Math.min(width / nx, height / ny)
+  return (
+    <div
+      className="flex flex-wrap relative inset-o mx-auto"
+      style={{ width: `${width}px`, height: `${height}px` }}
+    >
+      {characters.map((char, i) => (
+        <div
+          key={i}
+          className="flex p-1 absolute"
+          style={{
+            width: `${blockDimension}px`,
+            height: `${blockDimension}px`,
+            top:
+              blockDimension *
+              Math.floor(i / Math.ceil(characters.length / ny)),
+            left: blockDimension * (i % nx),
+          }}
+        >
+          <div className="flex m-auto w-full h-full bg-gray-300">
+            <span className="m-auto">{char}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
