163155ce34a33b433763f900a1dacd1a5d3c2295
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-12 01:59:44 +0200

    refactor: move page

--- dev/src/components/Pages/LoreViewPage.tsx
+++ dev/src/components/Pages/LoreViewPage.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react'
-import { GAME_DESCRIPTION, HISTORY } from '../fixtures/game'
-import { Description } from './Description'
+import { HISTORY } from '../../fixtures/game'
+import { Description } from '../Description'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface HistoryView {}
+export interface LoreViewPage {}
 
-export const HistoryView: FC<HistoryView> = () => {
+export const LoreViewPage: FC<LoreViewPage> = () => {
   return (
     <div>
       <Description header="History" content={HISTORY} />
