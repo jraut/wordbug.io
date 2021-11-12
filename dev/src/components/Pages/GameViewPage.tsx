7c5b8cbfcee949c307b9b5e16f5109ec4570313f
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-12 02:16:39 +0200

    feat: show grid on game page

--- dev/src/components/Pages/GameViewPage.tsx
+++ dev/src/components/Pages/GameViewPage.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { Description } from '../Description'
 import { GAME_DESCRIPTION } from '../../fixtures/game'
+import { GameArea } from '../GameArea'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface GameViewPage {}
@@ -9,6 +10,9 @@ export const GameViewPage: FC<GameViewPage> = () => {
   return (
     <div>
       <Description header="The game of Wordbug" content={GAME_DESCRIPTION} />
+      <div>
+        <GameArea />
+      </div>
     </div>
   )
 }
