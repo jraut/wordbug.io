dd64bcd204853cb977bdb8b14ad12932e678af82
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-12 14:02:49 +0200

    feat: fine tune grid resize animation

--- dev/tailwind.config.js
+++ dev/tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      transitionProperty: {
+        positioning: 'top, right, bottom, left',
+        spacing: 'margin, padding',
+        width: 'width',
+      },
     },
-    variants: {},
-    plugins: [],
+  },
+  variants: {},
+  plugins: [],
 }
