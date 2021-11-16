ee03d9632268db6ba0da34a2e84bbde68e41bfd3
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-16 18:11:18 +0200

    refactor: move code

---
+++ dev/src/fixtures/dialog.ts
@@ -0,0 +1,17 @@
+export const rugoDialogLines = {
+  Random: [
+    "I'll find a way to save us all.",
+    'My friends are here for me.',
+    "I'll fight for a new future.",
+    "I'm trying to save everyone.",
+    "We're not gonna give up.",
+    'This world needs a hero.',
+    "I will fight for the world's future.",
+    "There's a lot of people counting on me.",
+    "I'll be a hero to everyone.",
+    "I'm fighting for the sake of the world.",
+    "I won't let my friends down.",
+    "We'll find a way to save everyone.",
+    'My friends are here for me.',
+  ],
+}
