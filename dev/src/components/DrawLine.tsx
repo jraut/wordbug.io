d8d8d5733702cab61b864330b33ef74db3502e68
Juho Rautioaho <juho.rautioaho@gmail.com>
2021-11-21 14:48:23 +0200

    fix: better line drawing

--- dev/src/components/DrawLine.tsx
+++ dev/src/components/DrawLine.tsx
@@ -20,16 +20,28 @@ export const DrawLine: FC<DrawLine> = ({
 }) => {
   if (checkedIds.length > 0) {
     const halfBlock = blockSize / 2
-    const [_initialX, _initialY] = indexToCoordinate(checkedIds[0], dimensions)
-    const initialX = _initialX * blockSize + halfBlock
-    const initialY = _initialY * blockSize + halfBlock
-    const curvePath = `M ${initialX} ${initialY} Q ${initialX} ${initialY}, ${initialX} ${initialY} ${checkedIds
-      .slice(1)
-      .map((id) => {
+    const curvePath = checkedIds
+      .map((id, i) => {
         const [x, y] = indexToCoordinate(id, dimensions)
-        return `T ${x * blockSize + halfBlock} ${y * blockSize + halfBlock}`
+        if (i === 0) {
+          const initialX = x * blockSize + halfBlock
+          const initialY = y * blockSize + halfBlock
+          return `M ${initialX} ${initialY}`
+        } else {
+          const [_xPrev, _yPrev] = indexToCoordinate(
+            checkedIds[i - 1],
+            dimensions,
+          )
+          const xPrev = _xPrev * blockSize + halfBlock
+          const yPrev = _yPrev * blockSize + halfBlock
+          const xCur = x * blockSize + halfBlock
+          const yCur = y * blockSize + halfBlock
+          return `Q ${xPrev} ${yPrev}, ${(xPrev + xCur) / 2} ${
+            (yPrev + yCur) / 2
+          }`
+        }
       })
-      .join(' ')}`
+      .join(' ')
     return (
       <svg
         className="absolute full-w full-h pointer-events-none z-10"
@@ -37,7 +49,6 @@ export const DrawLine: FC<DrawLine> = ({
         height={height}
         xmlns="http://www.w3.org/2000/svg"
       >
-        <circle cx="10" cy="10" r="20" fill="red" />
         <path
           d={curvePath}
           stroke="rgba(0,0,0,0.333)"
