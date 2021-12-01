e46de003cc261ee8f8b3749dbdf492f81a42c789
Juho Rautioaho <juho@kasvulabs.com>
2021-12-01 22:56:57 +0200

    fix: make char portrait legible

--- dev/src/features/select-character/ExpandedDetails.tsx
+++ dev/src/features/select-character/ExpandedDetails.tsx
@@ -24,11 +24,14 @@ export const ExpandedDetails: React.FC<ExpandedDetails> = ({
       >
         X
       </button>
-      <div key={character.name} className="p-4">
+      <div key={character.name} className="p-4 max-h-96">
         <div className="flex flex-col p-2 pt-6 mx-auto space-y-4 bg-gray-400 rounded-md shadow-xl">
           <div
-            className="m-auto bg-contain rounded-md w-80 h-80"
-            style={{ backgroundImage: `url(${character.portrait})` }}
+            className="flex-shrink-0 m-auto bg-center bg-no-repeat bg-contain rounded-md w-80 h-80"
+            style={{
+              backgroundImage: `url(${character.portrait})`,
+              maxHeight: '30vh',
+            }}
           ></div>
           <h2 className="font-serif text-xl text-gray-800 shadow-sm font-large">
             {character.name}
@@ -45,7 +48,7 @@ export const ExpandedDetails: React.FC<ExpandedDetails> = ({
             Pick character and Play!
           </button>
 
-          <div className="items-center p-2 prose shadow-sm rounded-s">
+          <div className="items-center p-2 overflow-scroll prose shadow-sm rounded-s">
             <div>
               {character.description.map((d, i) => (
                 <p key={i}> {d}</p>
